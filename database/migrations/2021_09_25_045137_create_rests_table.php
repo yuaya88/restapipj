7642ae0c7766ef518982727dc559734b58dc14cd
aya <yuaya.seiga@gmail.com>
2021-09-26 16:42:40 +0900

    firstcommit

---
+++ database/migrations/2021_09_25_045137_create_rests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rests', function (Blueprint $table) {
+            $table->id();
+            $table->string('message');
+            $table->string('url');
+            $table->timestanp('created_at')->useCurrent()->nullable();
+            $table->timestamp('update_at')->useCrrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rests');
+    }
+}
